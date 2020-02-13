eb3de818c212220c8bbd3dfe9be118a6395a334d
Amber <amber.j.nola@gmail.com>
2020-02-13 10:50:18 -0600

    geolocation has been extracted from react native .60 version. working on solution

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
 import {FlatList} from 'react-native';
 import ForecastCard from './components/ForecastCard';
+navigator.geolocation = require('@react-native-community/geolocation');
+
 import {WEATHER_API_KEY} from 'react-native-dotenv';
 
 /*We'll be using 4 variables of state:
@@ -10,6 +12,7 @@ import {WEATHER_API_KEY} from 'react-native-dotenv';
  * - An error string indicating if there's been an error in the API response
  * And then initialise these in the constructor for the class
  */
+
 export default class App extends React.Component {
   constructor(props) {
     super(props);
@@ -22,11 +25,15 @@ export default class App extends React.Component {
     };
   }
 
+  componentDidMount() {
+    // Get the user's location
+    this.getLocation();
+  }
+
   getLocation() {
     // Get the current position of the user
     navigator.geolocation.getCurrentPosition(
       position => {
-        //sets the state of the latitude and longitude to the response
         this.setState(
           prevState => ({
             latitude: position.coords.latitude,
@@ -36,7 +43,6 @@ export default class App extends React.Component {
             this.getWeather();
           },
         );
-        // now that we've got the location of the user stored in the state of the application
       },
       error => this.setState({forecast: error.message}),
       {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000},
@@ -45,13 +51,8 @@ export default class App extends React.Component {
 
   getWeather() {
     // Construct the API url to call
-    let url =
-      'https://api.openweathermap.org/data/2.5/forecast?lat=' +
-      this.state.latitude +
-      '&lon=' +
-      this.state.longitude +
-      '&units=metric&appid=' +
-      WEATHER_API_KEY;
+    let url = `https://api.openweathermap.org/data/2.5/forecast?lat=${this.state.latitude}$&lon=${this.state.longitude}&units=metric&appid=7ba3a2da2c4b1e5b9e08d7c61c7866ed`;
+
     // Call the API, and set the state of the weather forecast
     fetch(url)
       .then(response => response.json())
@@ -66,8 +67,8 @@ export default class App extends React.Component {
     return (
       <FlatList
         data={this.state.forecast.list}
-        style={{marginTop:20}}
-        keyExtractor={item => item.dt_text}
+        style={{marginTop: 20}}
+        keyExtractor={item => item.dt_txt}
         renderItem={({item}) => (
           <ForecastCard
             detail={item}
